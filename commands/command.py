8f51b4aa7175cf42c52d6b64bec0b06c4d8faac2
Lokesh <lokeshsingh7695@gmail.com>
2023-08-06 08:39:25 +0530

    doubt

---
+++ commands/command.py
@@ -0,0 +1,53 @@
+# pip install django
+# python.exe -m pip install --upgrade pip
+
+# for creating Django Project
+# django-admin startproject mysite
+
+# python manage.py runserver -> setup with server
+
+
+# python manage.py -> give below command help
+
+# Available subcommands:
+
+# [auth]
+#     changepassword
+#     createsuperuser
+
+# [contenttypes]
+#     remove_stale_contenttypes
+
+# [django]
+#     check
+#     compilemessages
+#     createcachetable
+#     dbshell
+#     diffsettings
+#     dumpdata
+#     flush
+#     inspectdb
+#     loaddata
+#     makemessages
+#     makemigrations
+#     migrate
+#     optimizemigration
+#     sendtestemail
+#     shell
+#     showmigrations
+#     sqlflush
+#     sqlmigrate
+#     sqlsequencereset
+#     squashmigrations
+#     startapp
+#     startproject
+#     test
+#     testserver
+
+# [sessions]
+#     clearsessions
+
+# [staticfiles]
+#     collectstatic
+#     findstatic
+#     runserver
---
+++ home/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,HttpResponse
+from home.models import contact
+
+# Create your views here.
+def index(request):
+    context ={
+        "name":"Lokesh Singh",
+        "age":21
+    }
+    return render(request,"index.html",context)
+    # return HttpResponse("this is home page")
+
+
+def about(request):
+    return HttpResponse("this is About page")
+
+def services(request):
+    return HttpResponse("this is Services page")
+
+def contact(request):    
+    # Get Method
+    if(request.method =='POST'):
+        name = request.POST['name']
+        email = request.POST['email']
+        ins = contact(name=name,email=email)
+        ins.save()
+        print("The data is saved in database")
+     
+    return render(request,"contact.html")
+
+
+# def form(request):
+#     finalans = 0            
+#     # Get Method
+#     try:
+     
+#         n2 = request.GET['num1']
+#         n1 = request.GET['num2']
+#         finalans = int(n1)+int(n2)
+#         print(n1)
+#         print(n2)
+#     except:
+#         pass
+#     return render(request,"form.html",{'output':finalans})
+
+
+
